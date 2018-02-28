f088df3611838836e54ad23967ad528f5b003748
satinder.b.singh <satinder.b.singh@CPX-1ANQ7GK3PDZ.dir.svc.accenture.com>
2018-02-28 14:40:49 -0800

    Test Commit

---
+++ src/TestPackage/TestClass.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("File to be checked on the Github");
+	}
+
+}
