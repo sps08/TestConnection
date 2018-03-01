5a30c70d8a1a2d0506a3f66b6f274e49f0bed0d7
satinder.b.singh <satinder.b.singh@CPX-1ANQ7GK3PDZ.dir.svc.accenture.com>
2018-02-28 16:01:40 -0800

    Testing 2

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("File to be checked on the Github");
+		System.out.println("File to be checked on the Github 2nd time");
 	}
 
 }
