b7827f5a5ce58b5628cd09bcd5613a26d0750ce8
satinder.b.singh <satinder.b.singh@CPX-1ANQ7GK3PDZ.dir.svc.accenture.com>
2018-02-28 16:33:25 -0800

    partial implemention on displaying the  numbers

---
+++ src/TestPackage/DataCheck.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+public class DataCheck {
+	int data[] = {0, 44, 7};
+	
+ public void display(){
+	 for (int i=0; i< data.length; i++){
+		 System.out.println(data[i]);
+		 
+	 }
+ }
+
+}
