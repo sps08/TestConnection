0678348822a61cd06e2523ca8eefbde9d8975fa4
satinder.b.singh <satinder.b.singh@CPX-1ANQ7GK3PDZ.dir.svc.accenture.com>
2018-02-28 16:25:54 -0800

    testing new function Greetings

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -4,6 +4,11 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		displayGreetings();
+		
+	}
+	public static void displayGreetings(){
 		System.out.println("File to be checked on the Github");
 		System.out.println("File to be checked on the Github 2nd time");
 		
